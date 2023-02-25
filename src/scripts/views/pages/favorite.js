b2dc1ec04d7576593b84732fce43cb1c7b8c9aee
unknown <cndysteff@gmail.com>
2023-02-26 01:19:03 +0700

    Initial Commit

---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,35 @@
+import FavRestoIdb from '../../data/resto-idb';
+import restoCard from '../templates/resto-card';
+
+const Favorite = {
+  async render() {
+    return `
+      <div class="container">
+        <h2 class="title-container">Favorite Resto</h2>
+
+        <section id="fav-resto"></section>
+      </div>
+    `;
+  },
+
+  async afterRender() {
+    // get resto fav
+    const data = await FavRestoIdb.getAllResto();
+
+    const favRestoContainer = document.querySelector('#fav-resto');
+
+    // if no resto fav
+    if (data.length === 0) {
+      favRestoContainer.innerHTML = `
+        Empty 
+      `;
+    }
+
+    // display all fav resto
+    data.forEach((resto) => {
+      favRestoContainer.innerHTML += restoCard(resto);
+    });
+  },
+};
+
+export default Favorite;
